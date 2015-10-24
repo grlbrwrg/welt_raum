b8dc0b2adc734f1332d9f2b64beab9b902c1d66f
Walter <walter.rothenberger@googlemail.com>
2015-10-24 23:28:29 +0200

    Update settings.py

--- welt_raum/settings.py
+++ welt_raum/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'f!#75zpmmdk#y3^v696b-a5o1sdicn)1t+@gts)4p1*axem6n%'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
